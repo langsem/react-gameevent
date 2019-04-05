fe9413d4247ef7cd384fc6c0a038aff58dccc55e
langsem <nikolai_langsem@live.com>
2019-04-05 23:12:55 +0200

    final
    final

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './resources/styles.css';
+import { Element } from 'react-scroll';
+
+import Header from './components/header,footer/Header';
+import Featured from './components/Featured';
+import Info from './components/Info';
+import Highlight from './components/Highlights';
+import Pricing from './components/Pricing/index';
+import Footer from './components/header,footer/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{ height:"1500px"}}>
+        <Header />
+        <Element name="featured">
+        <Featured />
+        </Element>
+        
+        <Element name="venueinfo">
+        <Info />
+        </Element>
+        
+        <Element name="highlights">
+        <Highlight />
+        </Element>
+        
+        <Element name="pricing">
+        <Pricing />
+        </Element>
+        
+        <Footer />
+      </div>
+    );
+  }
+} 
+
+export default App;
---
+++ src/components/header,footer/SideDrawer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { scroller } from 'react-scroll';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+
+export default function SideDrawer (props)  {
+
+    const scrollToElement = (element) => {
+       scroller.scrollTo(element, {
+         duration: 1500,
+         delay: 100,
+         smooth: true
+       })
+       props.onClose(false)
+    }
+
+  return (
+       <Drawer
+           anchor="right"
+           open={props.open}
+           onClose={()=>props.onClose(false) }
+       >
+         <List component="nav">
+            <ListItem button onClick={()=> scrollToElement('featured')}>
+              Game Info
+            </ListItem>
+
+            <ListItem button onClick={()=> scrollToElement('venueinfo')}>
+              Event starts in
+            </ListItem>
+
+            <ListItem button onClick={()=> scrollToElement('highlights')}>
+              Highlights
+            </ListItem>
+
+            <ListItem button onClick={()=> scrollToElement('pricing')}>
+              Pricing
+            </ListItem>
+         </List>
+       </Drawer>
+    )
+}
+
